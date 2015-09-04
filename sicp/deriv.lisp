24d990d68f081bce24f56a8cf54fef38501315f7
rafaelbetatester <rafaelbetatester@gmail.com>
2015-09-03 23:15:49 -0300

    Adiciona algumas funções do Exercício 2.56
    Complemento do deriv.lisp do Leonardo -> Falta adicionar a cláusula na função deriv

--- sicp/deriv.lisp
+++ sicp/deriv.lisp
@@ -1,2 +1,16 @@
 (defun pair? (x)
   (and (not (atom x)) (not (null x))))
+
+(defun make-exponentiation (b e)
+  (cond ((=number? e 0) 1)
+	((=number? e 1) b)
+	(t (list '** b e))))
+
+(defun exponentiation? (x)
+  (and (pair? x) (eq (car x) '**)))
+
+(defun base (e)
+  (cadr e))
+
+(defun exponent (e)
+  (caddr e))
