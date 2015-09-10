04798ac9ed7cd67e4e766287cfd1b84127c63d49
rafaelbetatester <rafaelbetatester@gmail.com>
2015-09-10 15:43:49 -0300

    Update sentence-pt

--- paip/sentence-pt.lisp
+++ paip/sentence-pt.lisp
@@ -1,12 +1,14 @@
-(defun sentence ()    (append (noun-phrase) (verb-phrase)))
+(defun sentence () (append (noun-phrase) (verb-phrase)))
 (defun noun-phrase () (append (Noun)))
 (defun verb-phrase () (append (Verb) (noun-phrase)))
-(defun Article-masc ()     (one-of '(o um)))
-(defun Article-fem ()     (one-of '(a uma)))
-(defun Verb ()        (one-of '(viu elogiou abraçou traiu oprimiu ensinou idolatrou beijou)))
+(defun Article-masc () (one-of '(o um)))
+(defun Article-fem () (one-of '(a uma)))
+(defun Verb () (one-of '(viu elogiou abraçou traiu oprimiu ensinou idolatrou beijou)))
+
 (defun Noun () 
-  (if (= 0 (random 2)) (append (append (Article-masc)  (Noun-masc)) (Adj*-masc))
-	 (append (append (Article-fem)  (Noun-fem)) (Adj*-fem))))
+  (if (= 0 (random 2)) (append (Article-masc)  (Noun-masc) (Adj*-masc))
+      (append (Article-fem) (Noun-fem) (Adj*-fem))))
+
 (defun one-of (set)
   "Pick one element of set, and make a list of it."
   (list (random-elt set)))
@@ -16,16 +18,20 @@
   (elt choices (random (length choices))))
 
 
-(defun Adj*-fem () (if (= (random 2) 0)
-			nil
-			(append (Adj-fem) (Adj*-fem))))
+(defun Adj*-fem () 
+  (if (= (random 2) 0)
+      nil
+      (append (Adj-fem) (Adj*-fem))))
 
-(defun Adj*-masc () (if (= (random 2) 0)
-			nil
-			(append (Adj-masc) (Adj*-masc))))
-			
+(defun Adj*-masc ()
+  (if (= (random 2) 0)
+      nil
+      (append (Adj-masc) (Adj*-masc))))
+
 (defun Adj-masc () (one-of '(grande pequeno azul verde feio bonito chato)))
+
 (defun Adj-fem () (one-of '(grande pequena azul verde feia bonita chata)))
 
 (defun Noun-masc () (one-of '(homem cachorro gato predador militar matematico engenheiro mendigo psicologo lucas leonardo rafael fellipe astronauta caio otavio secretario medico professor garçom advogado neymar alexandre)))
+
 (defun Noun-fem () (one-of '(mulher cadela gata isabela secretaria esposa predadora engenheira psicologa medica professora kaline)))
